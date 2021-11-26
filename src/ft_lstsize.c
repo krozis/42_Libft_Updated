e54332d6e804cbdce50722fe65ebef0f4aa2bb8c
Stephen ELIE <stelie@student.42.fr>
2021-11-26 16:32:45 +0100

    corrected lst and *lst

--- src/ft_lstsize.c
+++ src/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:48:26 by stelie            #+#    #+#             */
-/*   Updated: 2021/11/24 17:59:47 by stelie           ###   ########.fr       */
+/*   Updated: 2021/11/26 15:25:21 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_lstsize(t_list *lst)
 	int	size;
 
 	size = 0;
-	if (*lst != NULL)
+	if (lst != NULL)
 		size++;
 	while (lst->next != NULL)
 	{
