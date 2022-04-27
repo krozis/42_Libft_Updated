82f60a45d191321beb4af077ace57b173985da00
krozis <stelie@student.42.fr>
2022-04-27 15:24:29 +0200

    added str_cut_before|after norm OK

--- src/05_strings/05_04.c
+++ src/05_strings/05_04.c
@@ -6,9 +6,11 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:02:15 by stelie            #+#    #+#             */
-/*   Updated: 2022/04/19 20:37:08 by krozis           ###   ########.fr       */
+/*   Updated: 2022/04/27 15:23:38 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_ctolower(int c)
 {
@@ -22,4 +24,42 @@ int	ft_ctoupper(int c)
 	if (c >= 'a' && c <= 'z')
 		return (c - 32);
 	return (c);
+}
+
+char	*ft_str_cut_before(char *src, char c)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i] && src[i] != c)
+		i++;
+	dest = malloc(sizeof(char) * (i));
+	if (dest == NULL)
+		return (NULL);
+	dest[i] = '\0';
+	while (--i >= 0)
+		dest[i] = src[i];
+	return (dest);
+}
+
+char	*ft_str_cut_after(char *src, char c)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (src[i] && src[i] != c)
+		i++;
+	dest = malloc(sizeof(char) * ((ft_strlen(src) - i) + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (src[++i])
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	return (dest);
 }
